90b3a0b8bb41dc87407a81fea7d915ba8cf4985b
Isaac Wooten <i.wooten@gmail.com>
2011-12-18 02:51:17 -0800

    updating precompiled assets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,5 +2,5 @@ CarrierWave.configure do |config|
   config.grid_fs_database = 'dub_leds_development'
   config.grid_fs_host = 'localhost'
   config.storage = :grid_fs 
-  config.grid_fs_access_url = "/images" 
+  config.grid_fs_access_url = "/tmp" 
 end
