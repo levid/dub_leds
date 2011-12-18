21c60a196d4712a84d2067eadabbd9e2aff04cf4
Isaac Wooten <i.wooten@gmail.com>
2011-12-18 04:19:45 -0800

    updating environment

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module DubLeds
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.orm :active_record
