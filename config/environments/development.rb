659a9e532f17d8823985044d15744dab99d9610f
Isaac <i.wooten@gmail.com>
2012-04-24 12:59:00 -0700

    updating s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 DubLeds::Application.configure do
   APP_CONFIG      = YAML.load_file(Rails.root.join('config', 'app.yml'))[Rails.env]
-  S3_CONFIG       = YAML.load_file(Rails.root.join('config', 'amazon_s3.yml'))[Rails.env]
+  S3_CONFIG       = YAML.load_file(Rails.root.join('config', 's3.yml'))[Rails.env]
   OMNIAUTH_CONFIG = YAML.load_file(Rails.root.join('config', 'omniauth.yml'))[Rails.env]
   
   # Settings specified here will take precedence over those in config/application.rb
