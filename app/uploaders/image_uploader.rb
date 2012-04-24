e3351df5a61421752037f87edf535d64cc50fa5c
Isaac <i.wooten@gmail.com>
2012-04-24 00:58:25 -0700

    fixing thumbs

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,9 +34,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   #     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   #   end
   
-  # version :thumb do
-  #     process :resize_to_fill => [80,80]
-  #   end
+  version :thumb do
+    process :resize_to_fill => [80,80]
+  end
   
   def store_dir
     'uploads'
